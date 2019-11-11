05d27963c3913d308afe731dbeffd0688ff05340
Max Wilkinson <mawilkin@umich.edu>
2019-11-11 11:28:03 -0500

    Made vscode import more explicit

--- src/widget.ts
+++ src/widget.ts
@@ -1,17 +1,14 @@
-import * as vscode from 'vscode';
+import { StatusBarItem, StatusBarAlignment, window } from 'vscode';
 
 export const StatusBarCommandId = 'extension.beginFluxTimer';
 
 export class Widget {
   private static instance: Widget;
 
-  statusBarItem: vscode.StatusBarItem;
+  statusBarItem: StatusBarItem;
 
   private constructor() {
-    this.statusBarItem = vscode.window.createStatusBarItem(
-      vscode.StatusBarAlignment.Left,
-      1
-    );
+    this.statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left, 1);
     this.statusBarItem.text = `$(watch) Click to Start Flux Timer`;
     this.statusBarItem.command = StatusBarCommandId;
     this.statusBarItem.show();
